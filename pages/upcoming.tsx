fd85253428e66657428bc8a6a551a0de2ca7fddc
Monkey D Zeke <monkeydzeke@protonmail.com>
2021-01-14 20:27:07 -0800

    updating upcoming mmgs

--- pages/upcoming.tsx
+++ pages/upcoming.tsx
@@ -58,6 +58,27 @@ export default function Upcoming() {
                   on Saturday, January 16th at 4:00 pm PST!</p>
                 <p><a href="https://opheron.leadingthebest.com/UMCW0001" target="_blank" rel="noreferrer">Register here!</a></p>
               </dd>
+              <dt>
+                <a href="https://opheron.leadingthebest.com/GMMW0001" target="_blank" rel="noreferrer">
+                  The Guide to Mind Mapping
+                </a>
+              </dt>
+              <dd>
+                <p><i>Improve Your Understanding by Mind Mapping</i></p>
+                <p>Do you have trouble sharing your thoughts in a way that others understand? Do
+                  you long for a better way to organize projects and plan ahead? Are you looking
+                  for a new way to collaboratively problem-solve?</p>
+                <p>If so, mind mapping is a great technique for you to learn and perfect.</p>
+                <p>Mind mapping is a tool that uses words, images, numbers, and colors to group
+                  together natural associations, generate more ideas, and find deeper meaning.
+                  Thanks to its intuitive design, you can generate ideas quicker, review
+                  information faster, and explore creative pathways that would be less obvious
+                  with lists, paragraphs of writing, or a drawn-out set of notes.</p>
+                <p>If you’re ready to learn a new way of organizing your thoughts and ideas into
+                  a visual graphic that you can share with others, you don’t want to miss out on
+                  my webinar, The Guide to Mind Mapping, on Saturday, February 20th at 4:00 pm PST.</p>
+                <p><a href="https://opheron.leadingthebest.com/GMMW0001" target="_blank" rel="noreferrer">Register here!</a></p>
+              </dd>
             </dl>
           </section>
           <section className={styles.mmgs} id="mmgs">
@@ -76,16 +97,6 @@ export default function Upcoming() {
                   <div className={styles.details}>
                     <dl>
                       <dt>Group One</dt>
-                      <dd><strong>Next Start Date:</strong> January 9th 2021</dd>
-                      <dd><strong>Time:</strong> Every Saturday 6-7pm PST</dd>
-                      <dd><strong>Location:</strong> Online via Zoom</dd>
-                      <dd><strong>Cost:</strong> Pay what you want!</dd>
-                      <dd><strong>Duration:</strong> 8 weeks</dd>
-                      <dd><strong>ONLY 10 SEATS AVAILABLE</strong></dd>
-                      <dd><a href="https://opheron.leadingthebest.com/MLGW0001" target="_blank" rel="noreferrer">Register Here</a></dd>
-                    </dl>
-                    <dl>
-                      <dt>Group Two</dt>
                       <dd><strong>Next Start Date:</strong> February 5th 2021</dd>
                       <dd><strong>Time:</strong> Every Friday 8-9am PST</dd>
                       <dd><strong>Location:</strong> Online via Zoom</dd>
@@ -93,6 +104,16 @@ export default function Upcoming() {
                       <dd><strong>Duration:</strong> 8 weeks</dd>
                       <dd><strong>ONLY 10 SEATS AVAILABLE</strong></dd>
                       <dd><a href="https://opheron.leadingthebest.com/MLGW0002" target="_blank" rel="noreferrer">Register Here</a></dd>
+                    </dl>
+                    <dl>
+                      <dt>Group Two</dt>
+                      <dd><strong>Next Start Date:</strong> March 13th 2021</dd>
+                      <dd><strong>Time:</strong> Every Saturday 6-7pm PST</dd>
+                      <dd><strong>Location:</strong> Online via Zoom</dd>
+                      <dd><strong>Cost:</strong> Pay what you want!</dd>
+                      <dd><strong>Duration:</strong> 8 weeks</dd>
+                      <dd><strong>ONLY 10 SEATS AVAILABLE</strong></dd>
+                      <dd><a href="https://opheron.leadingthebest.com/FINM0001" target="_blank" rel="noreferrer">Register Here</a></dd>
                     </dl>
                   </div>
                 </details>
@@ -109,17 +130,6 @@ export default function Upcoming() {
                   <summary>Group Details &amp; Options</summary>
                   <div className={styles.details}>
                     <dl>
-                      <dt>Group One</dt>
-                      <dd><strong>Next Start Date:</strong> January 8th 2021</dd>
-                      <dd><strong>Time:</strong> Every Friday 6-7pm PST</dd>
-                      <dd><strong>Location:</strong> Online via Zoom</dd>
-                      <dd><strong>Cost:</strong> <s>$278</s> $194.60</dd>
-                      <dd><strong>Duration:</strong> 10 weeks</dd>
-                      <dd><strong>ONLY 10 SEATS AVAILABLE</strong></dd>
-                      <dd><a href="https://opheron.leadingthebest.com/CL0001" target="_blank" rel="noreferrer">Register Here</a></dd>
-                    </dl>
-                    <dl>
-                      <dt>Group Two</dt>
                       <dd><strong>Next Start Date:</strong> Feburary 6th 2021</dd>
                       <dd><strong>Time:</strong> Every Saturday 8-9pm PST</dd>
                       <dd><strong>Location:</strong> Online via Zoom</dd>
@@ -132,36 +142,36 @@ export default function Upcoming() {
                 </details>
               </dd>
               <hr />
-              <dt>Everyone Communicates, Few Connect: Practices</dt>
+              <dt>Everyone Communicates, Few Connect</dt>
               <dd>
-                <p>We’ll mastermind on part two of Everyone Communicates Few Connect by John
-                  Maxwell, covering the 5 Connecting Practices. In this 5-week, in-depth process,
-                  you’ll join forces with a unique group of hand-selected, like-minded individuals
-                  focused on creating results in every area of their lives. Having the support and
-                  ideas of other ambitious people will allow you to gain a new perspective on
-                  leadership and adding value to your connection with others.</p>
+                <p>We’ll mastermind on Everyone Communicates Few Connect by John
+                  Maxwell. In this 10-week, in-depth process, you’ll join forces with a unique
+                  group of hand-selected, like-minded individuals focused on creating results
+                  in every area of their lives. Having the support and ideas of other ambitious
+                  people will allow you to gain a new perspective on leadership and adding value
+                  to your connection with others.</p>
                 <details>
                   <summary>Group Details &amp; Options</summary>
                   <div className={styles.details}>
                     <dl>
                       <dt>Group One</dt>
-                      <dd><strong>Next Start Date:</strong> January 12th 2021</dd>
+                      <dd><strong>Next Start Date:</strong> February 23rd 2021</dd>
                       <dd><strong>Time:</strong> Every Tuesday 8-9am PST</dd>
                       <dd><strong>Location:</strong> Online via Zoom</dd>
-                      <dd><strong>Cost:</strong> <s>$139</s> $97.30</dd>
-                      <dd><strong>Duration:</strong> 5 weeks</dd>
+                      <dd><strong>Cost:</strong> $278</dd>
+                      <dd><strong>Duration:</strong> 10 weeks</dd>
                       <dd><strong>ONLY 10 SEATS AVAILABLE</strong></dd>
-                      <dd><a href="https://opheron.leadingthebest.com/MECF0003" target="_blank" rel="noreferrer">Register Here</a></dd>
+                      <dd><a href="https://opheron.leadingthebest.com/ECCM0002" target="_blank" rel="noreferrer">Register Here</a></dd>
                     </dl>
                     <dl>
                       <dt>Group Two</dt>
-                      <dd><strong>Next Start Date:</strong> January 14th 2021</dd>
+                      <dd><strong>Next Start Date:</strong> April 1st 2021</dd>
                       <dd><strong>Time:</strong> Every Thursday 12-1pm PST</dd>
                       <dd><strong>Location:</strong> Online via Zoom</dd>
-                      <dd><strong>Cost:</strong>  <s>$139</s> $97.30</dd>
-                      <dd><strong>Duration:</strong> 5 weeks</dd>
+                      <dd><strong>Cost:</strong> $278</dd>
+                      <dd><strong>Duration:</strong> 10 weeks</dd>
                       <dd><strong>ONLY 10 SEATS AVAILABLE</strong></dd>
-                      <dd><a href="https://opheron.leadingthebest.com/MECF0004" target="_blank" rel="noreferrer">Register Here</a></dd>
+                      <dd><a href="https://opheron.leadingthebest.com/ECCM0001" target="_blank" rel="noreferrer">Register Here</a></dd>
                     </dl>
                   </div>
                 </details>
