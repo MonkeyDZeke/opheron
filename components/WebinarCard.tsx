250bcc0df059886d7270d3751b84808e68eea288
Morgan Love <morgan.j.love@gmail.com>
2021-11-22 14:33:16 -0600

    updating webinar info

--- components/WebinarCard.tsx
+++ components/WebinarCard.tsx
@@ -7,7 +7,7 @@ const WebinarCard = ({
   <div className={styles.card}>
     <h4>Sign Up For My Next Webinar!</h4>
     <script src="https://cdn.podia.com/embeds.js" async />
-    <LinkTo {...rest} href="https://courses.opheron.com/healthy-hybrid-communities" data-podia-embed="card">Healthy Hybrid Communities</LinkTo>
+    <LinkTo {...rest} href="https://courses.opheron.com/task-time-management-that-works" data-podia-embed="card">Task &amp; Time Management That Works</LinkTo>
   </div>
 )
 
